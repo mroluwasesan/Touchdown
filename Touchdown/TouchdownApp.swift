f797730bc4abf22a18518a13bf93c0d7f20df6f6
mroluwasesan <mr.oluwasesan@gmail.com>
2023-04-27 00:36:53 +0100

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Negatron on 27/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
