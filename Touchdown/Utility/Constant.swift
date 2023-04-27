0e8fa3c012f5f71b9b4dce220b5f6dbdc03ed25d
mroluwasesan <mr.oluwasesan@gmail.com>
2023-04-27 01:20:07 +0100

    Project Files SetUp and Footer View

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,32 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Negatron on 27/04/2023.
+//
+
+import SwiftUI
+
+// DATA
+
+
+// COLOR
+
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// LAYOUT
+
+
+
+// UX
+
+
+
+// API
+// IMAGE
+// FONT
+// STRING
+// MISC
+
+
