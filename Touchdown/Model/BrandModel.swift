e1acd88feead685e61051081d9d560d04fc4b43e
mroluwasesan <mr.oluwasesan@gmail.com>
2023-04-30 20:29:09 +0100

    Develop a horizontal grid layout for the brands

---
+++ Touchdown/Model/BrandModel.swift
@@ -0,0 +1,13 @@
+//
+//  BrandModel.swift
+//  Touchdown
+//
+//  Created by Negatron on 30/04/2023.
+//
+
+import Foundation
+
+struct Brand: Codable, Identifiable {
+  let id: Int
+  let image: String
+}
